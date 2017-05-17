9afd801c2182bac58f76729c19104adbe56f42fb
nialltcd <nihughes@tcd.ie>
2017-05-17 15:09:06 +0100

    Added neo4j database insertion

--- Servant/src/Lib.hs
+++ Servant/src/Lib.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE DataKinds       #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeOperators   #-}
+{-# LANGUAGE OverloadedStrings      #-}
+
 module Lib
     ( startApp
     , app
@@ -20,13 +22,17 @@ import GitHub.Data.Repos
 import qualified GitHub.Endpoints.Users as GitHubUsers
 import qualified GitHub.Endpoints.Repos as GitHubRepos
 import qualified GitHub.Endpoints.Users.Followers as GitHubFollowers
+import Database.Bolt
+import Data.Map
 
+config :: BoltCfg
+config = def { user = "neo4j", password = "neo4j"}
 
 type API = "crawl" :> Capture "user" String :> Get '[JSON] String
 
 
 startApp :: IO ()
-startApp = run 8080 app
+startApp = Network.Wai.Handler.Warp.run 8080 app
 
 app :: Application
 app = serve api server
@@ -42,8 +48,8 @@ crawlGithub :: String -> Handler String
 crawlGithub user = liftIO $ do
   -- repos <- getRepos (fromString user)
   userinfo <- getUserInfo (fromString user)
-  --addUser user
-  return userinfo
+  addUser user
+  return user
 
 
 getUserInfo :: Text -> IO User
@@ -54,3 +60,13 @@ getUserInfo name = do
         Left e -> error $ show e
         Right res -> return res
     return result
+
+-- CREATE (n:Person { name: 'Andres', title: 'Developer' })
+
+addUser :: String -> IO()
+addUser user = do
+    pipe <- connect config
+    result <- Database.Bolt.run pipe $ queryP "CREATE (n:User {name: {name}})" 
+                              (fromList [("name", T (fromString user))])
+    putStrLn $ show result
+    close pipe
