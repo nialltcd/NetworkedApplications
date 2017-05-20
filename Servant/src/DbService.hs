fa15c6247afa7632495f0fed822c1d9ffc47502b
nialltcd <nihughes@tcd.ie>
2017-05-20 23:01:56 +0100

    add user to db functionality

--- Servant/src/DbService.hs
+++ Servant/src/DbService.hs
@@ -5,4 +5,10 @@ import Data.Map
 config :: BoltCfg
 config = def { user = "neo4j", password = "neo4j"}
 
-
+addUser :: String -> IO()
+addUser user = do
+    pipe <- connect config
+    result <- Database.Bolt.run pipe $ queryP "CREATE (n:User {name: {name}})" 
+                              (fromList [("name", T (fromString user))])
+    putStrLn $ show result
+    close pipe
